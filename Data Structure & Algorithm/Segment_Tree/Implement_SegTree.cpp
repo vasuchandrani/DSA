aa8b64c906c7c470bc2d3a1f9a1a47796902e16d
Vatsal Chandrani <vasuchandrani1118@gmail.com>
2025-08-16 03:34:09 +0530

    update

--- Data Structure & Algorithm/Segment_Tree/Implement_SegTree.cpp
+++ Data Structure & Algorithm/Segment_Tree/Implement_SegTree.cpp
@@ -1,3 +1,5 @@
+// Range Sum query 
+
 #include<bits/stdc++.h>
 using namespace std;
 /*
@@ -26,23 +28,70 @@ Tree
 */
 // storage for segment tree:
 // vector<int> segTree;
-void buildSegTree(int idx, int l, int r, vector<int>& a, vector<int>& segTree) {
+
+// build Tree
+void buildSegTree(int node, int l, int r, vector<int>& a, vector<int>& segTree) {
 
     // base-case 
     if (l == r) {
-        segTree[idx] = a[l];
+        segTree[node] = a[l];
         return;
     }
 
     int mid = (l + r) /2;
     // go to left
-    buildSegTree(2*idx +1, l, mid, a, segTree);
+    buildSegTree(2*node +1, l, mid, a, segTree);
     // go to right
-    buildSegTree(2*idx +2, mid +1, r, a, segTree);
+    buildSegTree(2*node +2, mid +1, r, a, segTree);
 
-    segTree[idx] = segTree[2*idx +1] + segTree[2*idx +2];
+    segTree[node] = segTree[2*node +1] + segTree[2*node +2];
     //curr-Node    //left-Child        //right-Child
 }      
+
+// update Tree 
+// change or assign the value =val to index =i in array
+void update(int node, int i, int val, int l, int r, vector<int>& a, vector<int>& segTree) {
+
+    if (l == r) {
+        // if (l == node) {
+            a[i] = val;
+            segTree[node] = val;
+            return;
+        // }
+    }
+
+    int mid = (l + r)/2;
+
+    if (i <= mid) { //left 
+        update(2*node +1, i, val, l, mid, a, segTree);
+    }
+    else {// right
+        update(2*node +2, i, val, l, mid, a, segTree);
+    }
+
+    segTree[node] = segTree[2*node +1] + segTree[2*node +2];
+
+} 
+
+// Query
+int Query(int node, int s, int e, int l, int r, vector<int>& segTree) {
+    
+    // node is out of query 
+    if (s > r || e < l) {
+        return 0;
+    }
+
+    // node is in the query 
+    else if (l >= s && r <= e) {
+        return segTree[node];
+    }
+
+    // else {
+        int mid = (l + r)/2;
+        return Query(2*node +1, s, e, l, mid, segTree) + 
+                Query(2*node +2, s, e, mid +1, r, segTree);
+    // }
+}
 
 int main () {
     vector<int> arr = {1, 2, 7, 9, 10, 46, 2};
@@ -62,6 +111,24 @@ int main () {
     for (int i : segTree) {
         cout<<i<<" ";
     }cout<<endl;
+
+    cout<<"update Tree"<<endl;
+    update(0, 3, 10, 0, n-1, arr, segTree);
+
+    cout<<"Array"<<endl;
+    for (int i : arr) {
+        cout<<i<<" ";
+    }cout<<endl;
+
+    cout<<"Segment tree array: "<<endl;
+    for (int i : segTree) {
+        cout<<i<<" ";
+    }cout<<endl;
+    
+    int sum = 0;
+    sum += Query(0, 2, 6, 0, n-1, segTree);
+
+    cout<<sum<<endl;
 
     return 0;
 }
