abf8f4656e58e7a087f4af11e89035b9e09c5473
Vatsal Chandrani <vasuchandrani1118@gmail.com>
2025-08-21 01:04:53 +0530

    modifications

---
+++ Data Structure & Algorithm/#Projects/MazeSolverBFS/MazeSolverBFS.java
@@ -0,0 +1,311 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+
+public class MazeSolverBFS extends Frame {
+
+    private static final int CELL = 50;
+    private static final int SIDE = 650;
+
+    private int[][] maze;
+    private int rows, cols;
+
+    private int[] start;
+    private int[] end;
+
+    private Label msgLabel;
+    private final Button startBtn;
+    private Button pauseBtn;
+    private boolean isPaused = false;
+
+    private boolean[][] visited;
+    
+    private java.util.List<int[]> path;
+
+    private boolean showVisited = false;
+    private boolean showFinalPath = false;
+
+    public MazeSolverBFS(int[][] maze, int[] start, int[] end) {
+        
+        this.maze = maze;
+        rows = this.maze.length;
+        cols = this.maze[0].length;
+        this.start = start;
+        this.end = end;
+
+        setLayout(new BorderLayout());
+
+        Panel btnpanel = new Panel();
+        btnpanel.setLayout(new FlowLayout());
+
+        startBtn = new Button("Start BFS Traversal");
+        pauseBtn = new Button("Pause");
+
+        btnpanel.add(startBtn);
+        btnpanel.add(pauseBtn);
+
+        msgLabel = new Label("Click on Start to begin", Label.CENTER);
+        msgLabel.setFont(new Font("Arial", Font.BOLD, 18));
+
+        add(btnpanel, BorderLayout.NORTH);
+        add(msgLabel, BorderLayout.SOUTH);
+
+        startBtn.addActionListener(e -> {
+            
+            msgLabel.setText("BFS Traversing...");
+            new Thread(this::startVisualization).start();
+        });
+
+        pauseBtn.addActionListener(e -> {
+            
+            isPaused = !isPaused;
+            if (isPaused) {
+                
+                pauseBtn.setLabel("Resume");
+                msgLabel.setText("Visualization paused");
+            } 
+            else {
+               
+                pauseBtn.setLabel("Pause");
+                msgLabel.setText("BFS Traversing");
+            }
+        });
+
+        setTitle("Maze Solver Visualization - BFS");
+        setSize(SIDE, SIDE);
+        setVisible(true);
+
+        addWindowListener(new WindowAdapter() {
+            @Override 
+            public void windowClosing(WindowEvent we) {
+                System.exit(0);
+            }
+        });
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        int gridW = cols*CELL;
+        int gridH = rows*CELL;
+        int xPos = (SIDE - gridW)/2;
+        int yPos = (SIDE - gridH)/2;
+
+        // initial maze
+        for (int r=0; r<rows; r++) {
+            for (int c=0; c<cols; c++) {
+                
+                if (maze[r][c] == 1) {
+                    g.setColor(new Color(54, 69, 79));
+                }
+                else {
+                    g.setColor(Color.WHITE);
+                }
+                g.fillRect(xPos + c*CELL, yPos + r*CELL, CELL, CELL);
+                
+                g.setColor(Color.GRAY);
+                g.drawRect(xPos + c*CELL, yPos + r*CELL, CELL, CELL);
+            }
+        }
+
+        // traversal
+        if (showVisited && visited != null) {
+
+            g.setColor(new Color(135, 206, 250));
+            
+            for (int r = 0; r < rows; r++) {
+                for (int c = 0; c < cols; c++) {
+            
+                    if (visited[r][c] && !(r == start[0] && c == start[1]) && !(r == end[0] && c == end[1])) {
+                        
+                        g.fillRect(xPos + c*CELL, yPos + r*CELL, CELL, CELL);
+                        
+                        g.setColor(Color.GRAY);
+                        g.drawRect(xPos + c*CELL, yPos + r*CELL, CELL, CELL);
+                        
+                        g.setColor(new Color(135, 206, 250));
+                    }
+                }
+            }
+        }
+
+        // final path
+        if (showFinalPath && path != null) {
+            
+            g.setColor(new Color(34, 240, 95));
+            
+            for (int[] p : path) {
+            
+                int r = p[0], c = p[1];
+                if (!(r == start[0] && c == start[1]) && !(r == end[0] && c == end[1])) {
+            
+                    g.fillRect(xPos + c*CELL, yPos + r*CELL, CELL, CELL);
+                    
+                    g.setColor(Color.GRAY);
+                    g.drawRect(xPos + c*CELL, yPos + r*CELL, CELL, CELL);
+                    
+                    g.setColor(new Color(34, 240, 95));
+                }
+            }
+        }
+
+        g.setColor(new Color(0, 128, 255));
+        g.fillRect(xPos + start[1]*CELL, yPos + start[0]*CELL, CELL, CELL);
+
+        g.setColor(Color.RED);
+        g.fillRect(xPos + end[1]*CELL, yPos + end[0]*CELL, CELL, CELL);
+    }
+
+    private void delay(long ms) {
+        try {
+            Thread.sleep(ms);
+            while (isPaused) {
+                Thread.sleep(100);
+            }    
+        } 
+        catch (InterruptedException ie) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    private void startVisualization() {
+        
+        visited = new boolean[rows][cols];
+        path = new ArrayList<>();
+        
+        showVisited = true;
+        showFinalPath = false;
+
+        msgLabel.setText("BFS started");
+
+        boolean found = bfs();
+
+        if (found) {
+            
+            msgLabel.setText("BFS complete: Path found.");
+            delay(2000);
+            
+            showVisited = false;
+            showFinalPath = true;
+            
+            repaint();
+            msgLabel.setText("Final path highlighted");
+        } else {
+            
+            msgLabel.setText("BFS complete: No path found");
+            delay(2000);
+            
+            showVisited = true;
+            showFinalPath = false;
+
+            repaint();
+            msgLabel.setText("No path exists for this maze");
+        }
+    }
+
+    private boolean bfs() {
+        
+        Queue<int[]> queue = new LinkedList<>();
+        Map<String, int[]> parent = new HashMap<>();
+
+        queue.add(start);
+        
+        visited[start[0]][start[1]] = true;
+
+        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+
+        while (!queue.isEmpty()) {
+        
+            int[] cur = queue.poll();
+            int r = cur[0], c = cur[1];
+
+            repaint();
+            delay(750);
+
+            if (r == end[0] && c == end[1]) {
+                
+                buildPath(parent, end);
+                return true;
+            }
+
+            for (int[] d : dirs) {
+                
+                int nr = r + d[0], nc = c + d[1];
+                if (nr >= 0 && nr < rows && nc >= 0 && nc < cols && maze[nr][nc] == 0 && !visited[nr][nc]) {
+                
+                    queue.add(new int[]{nr, nc});
+                    visited[nr][nc] = true;
+                    parent.put(nr + "," + nc, cur);
+                }
+            }
+        }
+        return false;
+    }
+
+    private void buildPath(Map<String, int[]> parent, int[] end) {
+        
+        ArrayList<int[]> revPath = new ArrayList<>();
+        
+        int[] cur = end;
+        
+        while (cur != null) {
+        
+            revPath.add(cur);
+            cur = parent.get(cur[0] + "," + cur[1]);
+        }
+        
+        Collections.reverse(revPath);
+        path = revPath;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter rows: ");
+        int r = sc.nextInt();
+
+        System.out.print("Enter cols: ");
+        int c = sc.nextInt();
+
+        int[][] mazeInput = new int[r][c];
+        
+        int[] start = null;
+        int[] end = null;
+        int start_end = 0; // count of -1
+
+        System.out.println("Enter maze (0=open, 1=wall, -1=start/end):");
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+        
+                int val = sc.nextInt();
+        
+                if (val == -1) {
+                    start_end++;
+        
+                    if (start_end == 1) {
+                        start = new int[]{i, j};
+                    } 
+                    else if (start_end == 2) {
+                        end = new int[]{i, j};
+                    } 
+                    else {
+                        System.out.println("Error: More than 2 -1 values entered.");
+                        return;
+                    }
+                    
+                    mazeInput[i][j] = 0; 
+                } 
+                else {
+                    mazeInput[i][j] = val;
+                }
+            }
+        }
+
+        if (start_end < 2) {
+            System.out.println("Error: enter exactly 2 -1 values for start and end." + start_end);
+            return;
+        }
+
+        new MazeSolverBFS(mazeInput, start, end);
+    }
+}
--- Data Structure & Algorithm/#Projects/NQueen/NQueenVisualization.java
+++ Data Structure & Algorithm/#Projects/NQueen/NQueenVisualization.java
@@ -7,18 +7,19 @@ public class NQueenVisualization extends Frame {
     
     private static final int CELL = 50;
     private static final int SIDE = 650;
+    
     private int N; 
     private int[][] board; 
-    private Label messageLabel;
-    private TextField inputField;
-    private final Button startButton;
-    private Button pauseButton;
+    
+    private Label msgLabel;
+    private TextField inputText;
+    private final Button startBtn;
+    private Button pauseBtn;
     private boolean isPaused = false; 
 
     // stores all the solutions
     private List<int[][]> solutions; 
 
-    @SuppressWarnings("unused")
     public NQueenVisualization() {
 
         board = new int[4][4]; 
@@ -29,52 +30,56 @@ public class NQueenVisualization extends Frame {
         setLayout(new BorderLayout());
 
         // control panel for buttons and input-field
-        Panel controlPanel = new Panel();
-        controlPanel.setLayout(new FlowLayout());
+        Panel btnPanel = new Panel();
+        btnPanel.setLayout(new FlowLayout());
 
         Label inputLabel = new Label("Enter Queen:");
-        inputField = new TextField("4", 2); 
-        startButton = new Button("Start");
-        pauseButton = new Button("Pause");
+        inputText = new TextField("4", 2); 
+        startBtn = new Button("Start");
+        pauseBtn = new Button("Pause");
 
-        controlPanel.add(inputLabel);
-        controlPanel.add(inputField);
-        controlPanel.add(startButton);
-        controlPanel.add(pauseButton);
+        btnPanel.add(inputLabel);
+        btnPanel.add(inputText);
+        btnPanel.add(startBtn);
+        btnPanel.add(pauseBtn);
 
         // message panel  
-        messageLabel = new Label("Click 'Start' to begin", Label.CENTER);
-        messageLabel.setFont(new Font("Arial", Font.BOLD, 24)); 
+        msgLabel = new Label("Click on Start to begin", Label.CENTER);
+        msgLabel.setFont(new Font("Arial", Font.BOLD, 24)); 
 
-        add(messageLabel, BorderLayout.SOUTH);
-        add(controlPanel, BorderLayout.NORTH);
+        add(msgLabel, BorderLayout.SOUTH);
+        add(btnPanel, BorderLayout.NORTH);
 
         // add action listners
-        startButton.addActionListener(e -> {
+        startBtn.addActionListener(e -> {
+            
             try {
-                int input = Integer.parseInt(inputField.getText());
+                int input = Integer.parseInt(inputText.getText());
+            
                 if (input > 0) {
                     N = input;
                     board = new int[N][N];
                     
                     solutions.clear(); 
-                    messageLabel.setText("Visualization started");
+                    msgLabel.setText("Visualization started");
                     new Thread(this::startVisualization).start();
-                } else {
-                    messageLabel.setText("Invalid input! Please enter a valid number");
+                } 
+                else {
+                    msgLabel.setText("Invalid input! Please enter a valid number");
                 }
             } catch (NumberFormatException ex) {
-                messageLabel.setText("Invalid input! Please enter a valid number");
+                msgLabel.setText("Invalid input! Please enter a valid number");
             }
         });
-        pauseButton.addActionListener(e -> {
+        pauseBtn.addActionListener(e -> {
             isPaused = !isPaused; 
             if (isPaused) {
-                pauseButton.setLabel("Resume");
-                messageLabel.setText("Visualization paused");
-            } else {
-                pauseButton.setLabel("Pause");
-                messageLabel.setText("Visualization resumed");
+                pauseBtn.setLabel("Resume");
+                msgLabel.setText("Visualization paused");
+            } 
+            else {
+                pauseBtn.setLabel("Pause");
+                msgLabel.setText("Visualization started");
             }
         });
 
@@ -92,20 +97,21 @@ public class NQueenVisualization extends Frame {
     // ------
     @Override
     public void paint(Graphics g) {
-        int boardSize = N * CELL; 
-        int xOffset = (SIDE - boardSize) / 2; 
-        int yOffset = (SIDE - boardSize) / 2; 
+        int boardSize = N*CELL; 
+        int xPos = (SIDE - boardSize) / 2; 
+        int yPos = (SIDE - boardSize) / 2; 
 
         // paint board 
-        for (int row = 0; row < N; row++) {
-            for (int col = 0; col < N; col++) {
+        for (int row=0; row<N; row++) {
+            for (int col=0; col<N; col++) {
+                
                 g.setColor((row + col) % 2 == 0 ? new Color(54, 69, 79) : new Color(135, 206, 250));
-                g.fillRect(xOffset + col * CELL, yOffset + row * CELL, CELL, CELL);
+                g.fillRect(xPos + col*CELL, yPos + row*CELL, CELL, CELL);
 
                 // Queen 
                 if (board[row][col] == 1) {
                     g.setColor(Color.WHITE);
-                    g.fillOval(xOffset + col * CELL + 10, yOffset + row * CELL + 10, CELL - 20, CELL - 20);
+                    g.fillOval(xPos + col*CELL + 10, yPos + row*CELL + 10, CELL - 20, CELL - 20);
                 }
             }
         }
@@ -123,17 +129,22 @@ public class NQueenVisualization extends Frame {
     }
     // ------   NQueen solve
     private boolean isSafe(int row, int col) {
+        // same col
         for (int i = 0; i < col; i++) {
             if (board[row][i] == 1) return false;
         }
+        
+        // diagonal
         for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
             if (board[i][j] == 1) return false;
         }
         for (int i = row, j = col; i < N && j >= 0; i++, j--) {
             if (board[i][j] == 1) return false;
         }
+
         return true;
     }
+    
     private void findAllSolutions(int col) {  
      
         // base-case
@@ -142,12 +153,14 @@ public class NQueenVisualization extends Frame {
             for (int i = 0; i < N; i++) {
                 System.arraycopy(board[i], 0, solution[i], 0, N);
             }
+    
             solutions.add(solution);
             repaint();
+            
             try {
-                messageLabel.setText("Solution Found");
+                msgLabel.setText("Solution Found");
                 Thread.sleep(1500);
-                messageLabel.setText("Check for further solution"); 
+                msgLabel.setText("Check for further solution"); 
                 Thread.sleep(750); 
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
@@ -171,7 +184,7 @@ public class NQueenVisualization extends Frame {
     // -----
     private void startVisualization() {
         findAllSolutions(0);
-        messageLabel.setText("Found " + solutions.size() + " solutions.");
+        msgLabel.setText("Found " + solutions.size() + " solutions.");
         displaySolutions(); // Call the displaySolutions method after finding all solutions
     }
     // -----
@@ -182,13 +195,13 @@ public class NQueenVisualization extends Frame {
             repaint();
             try {
                 if (count == 1) {
-                    messageLabel.setText("Displaying " + count + "st solution...");
+                    msgLabel.setText("Displaying " + count + "st solution...");
                 } else if (count == 2) {
-                    messageLabel.setText("Displaying " + count + "nd solution...");
+                    msgLabel.setText("Displaying " + count + "nd solution...");
                 } else if (count == 3) {
-                    messageLabel.setText("Displaying " + count + "rd solution...");
+                    msgLabel.setText("Displaying " + count + "rd solution...");
                 } else {
-                    messageLabel.setText("Displaying " + count + "th solution...");
+                    msgLabel.setText("Displaying " + count + "th solution...");
                 }
                 Thread.sleep(3000); 
             } catch (InterruptedException e) {
@@ -198,9 +211,9 @@ public class NQueenVisualization extends Frame {
         }
         count--;
         if (count > 0)
-            messageLabel.setText("All solutions displayed, "+count+" solutions found for "+N+" Queens"); 
+            msgLabel.setText("All solutions displayed, "+count+" solutions found for "+N+" Queens"); 
         else 
-            messageLabel.setText("No solutions found for "+N+" Queens");
+            msgLabel.setText("No solutions found for "+N+" Queens");
     }
 
     public static void main(String[] args) {
